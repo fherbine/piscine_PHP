9766c37ce45bb14e7d58368813a8bf1727f2fa06
fherbine <felix.herbinet@yahoo.com>
2018-03-27 10:51:10 +0200

    Adding ex02 - a little program to see if a number is odd or even

---
+++ day_01/ex02/oddeven.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php
+	$stdin = fopen('php://stdin', 'r');
+	echo "Entrez un nombre: ";
+	$nb = fgets($stdin);
+	while ($nb)
+	{
+		$n = explode("\n", $nb);
+		if (is_numeric($n[0]))
+		{
+			$n[0] = (int)$n[0];
+			if ($n[0] % 2 == 0)
+				echo "Le chiffre ".$n[0]." est Pair\n";
+			else
+				echo "Le chiffre ".$n[0]." est Impair\n";
+		}
+		else
+			echo "'".$n[0]."' n'est pas un chiffre\n";
+		echo "Entrez un nombre: ";
+		$nb = fgets($stdin);
+	}
+	fclose($stdin);
+	echo "\n";
+?>
