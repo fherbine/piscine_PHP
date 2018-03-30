7be5a5a1e66992122c5efcfa195efe5672e81c2c
fherbine <felix.herbinet@yahoo.com>
2018-03-30 16:05:45 +0200

    Adding ex01

---
+++ day_04/ex01/create.php
@@ -0,0 +1,32 @@
+<?php
+	if ($_POST && $_POST['submit'] == "OK" && $_POST['login'] !== "" && $_POST['passwd'] !== "")
+	{
+		$var = true;
+		if (file_exists("../private") == false)
+			mkdir("../private", 0755, true);
+		else
+		{
+			$file = file_get_contents("../private/passwd");
+			if ($file)
+			{
+				$db = unserialize($file);
+				foreach ($db as $account)
+				{
+					if ($account['login'] == $_POST['login'])
+					{
+						echo "ERROR\n";
+						$var = false;
+					}
+				}
+			}
+		}
+		if ($var === true)
+		{
+			$db[] = array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd']));
+			file_put_contents("../private/passwd", serialize($db));
+			echo "OK\n";
+		}
+	}
+	else
+		echo "ERROR\n";
+?>
